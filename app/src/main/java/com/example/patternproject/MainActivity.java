314296e8a4f27246efc754f0257c1f0cbdefede9
RobertSchierz <36508029+RobertSchierz@users.noreply.github.com>
2020-06-11 16:05:52 +0200

    Created the recycler view

--- app/src/main/java/com/example/patternproject/MainActivity.java
+++ app/src/main/java/com/example/patternproject/MainActivity.java
@@ -1,14 +1,70 @@
 package com.example.patternproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    //vars
+    private ArrayList<String> mPatternNames = new ArrayList<>();
+    private ArrayList<Integer> mPatternImagePaths = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "oncreate started.");
+
+        initImageBitmaps();
+
+
+    }
+
+    private void initImageBitmaps(){
+
+        mPatternNames.add("Pattern");
+        mPatternImagePaths.add(R.drawable.pattern);
+
+        mPatternNames.add("Bubble Sort");
+        mPatternImagePaths.add(R.drawable.algorithm);
+
+        mPatternNames.add("Insertion Sort");
+        mPatternImagePaths.add(R.drawable.algorithm);
+
+        mPatternNames.add("Selection Sort");
+        mPatternImagePaths.add(R.drawable.algorithm);
+
+        mPatternNames.add("Merge Sort");
+        mPatternImagePaths.add(R.drawable.algorithm);
+
+        mPatternNames.add("Heapsort Sort");
+        mPatternImagePaths.add(R.drawable.algorithm);
+
+        mPatternNames.add("Quicksort Sort");
+        mPatternImagePaths.add(R.drawable.algorithm);
+
+
+
+        initPatternRecyclerView();
+    }
+
+    private void initPatternRecyclerView(){
+        Log.d(TAG, "init Patternlist");
+
+        RecyclerView recyclerView = findViewById(R.id.patternlist_recyclerview);
+        PatternlistAdapter adapter = new PatternlistAdapter(mPatternNames, mPatternImagePaths, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
--- app/src/main/java/com/example/patternproject/PatternlistAdapter.java
+++ app/src/main/java/com/example/patternproject/PatternlistAdapter.java
@@ -1,21 +1,75 @@
 package com.example.patternproject;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import de.hdodenhof.circleimageview.CircleImageView;
+import com.bumptech.glide.Glide;
 
-public class PatternlistAdapter {
+import java.io.File;
+import java.util.ArrayList;
+
+
+
+public class PatternlistAdapter extends RecyclerView.Adapter<PatternlistAdapter.ViewHolder>{
 
     private static final String TAG = "PatternlistAdapter";
 
+    private ArrayList<String> mPatternNames = new ArrayList<>();
+    private ArrayList<Integer> mPatternImages = new ArrayList<>();
+    private Context mContext;
+
+    public PatternlistAdapter(ArrayList<String> mPatternImageNames, ArrayList<Integer> mPatternImages, Context mContext) {
+        this.mPatternNames = mPatternImageNames;
+        this.mPatternImages = mPatternImages;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_patternlistitem, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called.");
+
+        Glide.with(mContext)
+                .asBitmap()
+                .load(mPatternImages.get(position))
+                .into(holder.image);
+
+        holder.imageName.setText(mPatternNames.get(position));
+
+        holder.patternListLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "clicked on " + mPatternNames.get(position));
+                Toast.makeText(mContext, mPatternNames.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPatternNames.size();
+    }
+
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        CircleImageView image;
+        ImageView image;
         TextView imageName;
         RelativeLayout patternListLayout;
 
